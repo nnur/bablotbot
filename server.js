7b77af6f067123a264f4623d64c3eafe13c46e9b
Naila <23naila.n@gmail.com>
2016-08-11 23:59:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ app.use(bodyParser.json())
 app.get('/', function (req, res) {
     res.send('Hello world, I am a chat bot')
 })
+//yes
 
-// for Facebook verification
+// for facebook verification
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
         res.send(req.query['hub.challenge'])
